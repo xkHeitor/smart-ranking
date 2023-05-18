1e56d833f60305f41315c1dfb45f6bd43f6a32f7
xkheitor <heitorcarvalhorod@gmail.com>
2023-05-18 17:05:37 -0300

    feat: added category initial module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PlayerModule } from './player/player.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryModule } from './category/category.module';
 import Environments from './config/environment';
 
 const Envs = new Environments();
 @Module({
-  imports: [MongooseModule.forRoot(Envs.getDbConnection()), PlayerModule],
+  imports: [
+    MongooseModule.forRoot(Envs.getDbConnection()),
+    PlayerModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
