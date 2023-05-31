58cd64e7ed5ff435c9013a7b7de534aaaba6ffec
xkheitor <heitorcarvalhorod@gmail.com>
2023-05-31 14:09:59 -0300

    feat: added initial implementation for micro-admin-backend

---
+++ apps/micro-admin-backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroAdminBackendModule } from './micro-admin-backend.module';
+import { Transport } from '@nestjs/microservices';
+import { config } from 'dotenv';
+
+(async function bootstrap() {
+  config();
+  const rabbitmqURL = `${process.env.DB_CONNECT_RABBITMQ}/${process.env.DB_CONNECT_RABBITMQ_VHOST}`;
+  const queueName = process.env.QUEUE_NAME;
+  const app = await NestFactory.createMicroservice(MicroAdminBackendModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [rabbitmqURL],
+      queue: queueName,
+    },
+  });
+  await app.listen();
+})();
