d2a990e28dc6e3c1bc9a7f027901f3b08d30a8ff
xkheitor <heitorcarvalhorod@gmail.com>
2023-06-09 13:00:48 -0300

    refactor: refactored logging interceptor

--- apps/api-gateway/src/infra/interceptors/logging.interceptor.ts
+++ apps/api-gateway/src/infra/interceptors/logging.interceptor.ts
@@ -1,15 +1,23 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
+import * as momentTimezone from 'moment-timezone';
 
+@Injectable()
 export default class LoggingInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    const dateNow = Date.now();
-    console.log(`Start... ${new Date()}`);
+    const getDate = () =>
+      momentTimezone.tz('America/Sao_Paulo').format('HH:mm:ss DD-MM-YYYY');
+    console.log(`Start... ${getDate()}`);
     return next
       .handle()
-      .pipe(tap(() => console.log(`Finish in... ${Date.now() - dateNow}ms`)));
+      .pipe(tap(() => console.log(`Finish in... ${getDate()}`)));
   }
 }
