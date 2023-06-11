b29623e682c5b8fc01b1dd17f2ea64bc053153eb
xkheitor <heitorcarvalhorod@gmail.com>
2023-06-10 21:19:25 -0300

    queue

--- apps/common/src/queue/queue.interface.ts
+++ apps/common/src/queue/queue.interface.ts
@@ -2,4 +2,5 @@ export default abstract class Queue {
   abstract connect(rabbitmqUrl: string, queue: string): void;
   abstract emitter(pattern: string, data: any): Promise<any>;
   abstract sender(pattern: string, data: any): Promise<any>;
+  abstract finalizeMsg(callback: any, args: any, context: any): Promise<any>;
 }
--- apps/common/src/queue/rabbitmq.adapter.ts
+++ apps/common/src/queue/rabbitmq.adapter.ts
@@ -1,12 +1,16 @@
 import {
   ClientProxy,
   ClientProxyFactory,
+  RmqContext,
   Transport,
 } from '@nestjs/microservices';
 import Queue from './queue.interface';
+import { Logger } from '@nestjs/common';
 
 export default class RabbitMQAdapter implements Queue {
   private clientAdminBackend: ClientProxy;
+  private ackErrors: string[] = ['E11000'];
+  private logger = new Logger('RabbitMQ');
 
   connect(rabbitmqUrl: string, queue: string): void {
     this.clientAdminBackend = ClientProxyFactory.create({
@@ -19,12 +23,35 @@ export default class RabbitMQAdapter implements Queue {
   }
 
   async emitter(pattern: string, data: any): Promise<any> {
-    console.info(`RabbitMQ - Emit for pattern: ${pattern}`);
+    this.logger.log(`RabbitMQ - Emit for pattern: ${pattern}`);
     return this.clientAdminBackend.emit(pattern, data);
   }
 
   async sender(pattern: string, data: any): Promise<any> {
-    console.info(`RabbitMQ - Send for pattern: ${pattern}`);
+    this.logger.log(`RabbitMQ - Send for pattern: ${pattern}`);
     return this.clientAdminBackend.send(pattern, data);
+  }
+
+  async finalizeMsg(
+    callback: any,
+    args: any,
+    context: RmqContext,
+  ): Promise<any> {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      const response = await callback(...args);
+      await channel.ack(originalMsg);
+      return response;
+    } catch (error: any) {
+      this.logger.error(`error:  ${error.message}`);
+      for (const ackError of this.ackErrors) {
+        if (error?.message.includes(ackError)) {
+          await channel.ack(originalMsg);
+          break;
+        }
+      }
+    }
   }
 }
