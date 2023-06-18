8efd8186a36e4a74bc5727204fa7cf2afdbb717e
xkheitor <heitorcarvalhorod@gmail.com>
2023-06-18 01:14:48 -0300

    refactor: api-gateway main module refactored

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,8 +1,18 @@
+import { Configs } from '@configs';
 import { Module } from '@nestjs/common';
-import { CategoryModule } from './category/category.module';
-import { PlayerModule } from './player/player.module';
+import { ConfigModule } from '@nestjs/config';
+import { QueueModule } from '@queue';
+import CategoryController from './category/category.controller';
+import { PlayerController } from './player/player.controller';
 
 @Module({
-  imports: [PlayerModule, CategoryModule],
+  controllers: [PlayerController, CategoryController],
+  imports: [
+    ConfigModule.forRoot({
+      load: Configs,
+      envFilePath: ['.env'],
+    }),
+    QueueModule,
+  ],
 })
 export class AppModule {}
