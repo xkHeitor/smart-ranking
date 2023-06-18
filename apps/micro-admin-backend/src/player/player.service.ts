9556a47d86a992025ea040d27a9dcc89de0c0b5c
xkheitor <heitorcarvalhorod@gmail.com>
2023-06-18 01:11:02 -0300

    feat: player service implemented

--- apps/micro-admin-backend/src/player/player.service.ts
+++ apps/micro-admin-backend/src/player/player.service.ts
@@ -1,4 +1,7 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import CreatePlayerDto from './dtos/create-player.dto';
+import UpdatePlayerDto from './dtos/update-player.dto';
+import Player from './interfaces/player.interface';
 import PlayerRepository from './repositories/player.repository';
 
 @Injectable()
@@ -6,4 +9,30 @@ export class PlayerService {
   private readonly logger = new Logger(PlayerService.name);
 
   constructor(private readonly playerRepository: PlayerRepository) {}
+
+  async getAllPlayers(): Promise<Player[]> {
+    return this.playerRepository.getAll();
+  }
+
+  async getPlayerById(id: string): Promise<Player | undefined> {
+    const foundPlayer: Player = await this.playerRepository.getFindById(id);
+    if (!foundPlayer) throw new NotFoundException(`Player not found`);
+    return foundPlayer;
+  }
+
+  async createPlayer(playerDto: CreatePlayerDto): Promise<any> {
+    return this.playerRepository.create(playerDto);
+  }
+
+  async updatePlayer(id: string, playerDto: UpdatePlayerDto): Promise<any> {
+    const foundPlayer: Player = await this.playerRepository.getFindById(id);
+    if (!foundPlayer) throw new NotFoundException('Player not found');
+    return this.playerRepository.update(id, playerDto);
+  }
+
+  async deletePlayer(id: string): Promise<void> {
+    const foundPlayer: Player = await this.playerRepository.getFindById(id);
+    if (!foundPlayer) throw new NotFoundException(`Player not found`);
+    return this.playerRepository.delete(id);
+  }
 }
