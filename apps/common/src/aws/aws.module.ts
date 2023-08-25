1b7d1dd61b6a8341bd3f70684ce45319cc8b58ac
xkheitor <heitorcarvalhorod@gmail.com>
2023-08-25 10:46:56 -0300

    fix: fixed in aws module

--- apps/common/src/aws/aws.module.ts
+++ apps/common/src/aws/aws.module.ts
@@ -1,7 +1,15 @@
+import { Configs } from '@configs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AwsService } from './aws.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: Configs,
+      envFilePath: ['.env'],
+    }),
+  ],
   providers: [AwsService],
   exports: [AwsService],
 })
