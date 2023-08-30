8aeb285e8cf2b5d65b3b8af0f76d2edf46e1610e
xkheitor <heitorcarvalhorod@gmail.com>
2023-08-30 13:07:52 -0300

    chore: added new aws configs

--- apps/common/src/configs/aws.config.ts
+++ apps/common/src/configs/aws.config.ts
@@ -1,7 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('aws', () => ({
+  endpoint_s3: process.env.AWS_S3_ENDPOINT,
+  bucket: process.env.AWS_BUCKET,
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  access_key: process.env.AWS_ACCESS_KEY_ID,
+  secret_key: process.env.AWS_SECRET_ACCESS_KEY,
 }));
